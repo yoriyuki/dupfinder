10e4a543efb7153d47bb3bf7de3bad1724d2b0e0
yoriyuki <yoriyuki.y@gmail.com>
2018-03-15 18:10:06 +0900

    improve performance

--- dupfind.py
+++ dupfind.py
@@ -1,7 +1,7 @@
 import os
+import sys
 import argparse
 import re
-import json
 import shellinford
 import pygtrie
 from tqdm import tqdm
@@ -10,7 +10,7 @@ parser = argparse.ArgumentParser(description='Search duplicates from the index')
 parser.add_argument('suffix', metavar='S', help = 'filename suffix')
 parser.add_argument('dir', metavar='D', help='search directory')
 parser.add_argument('-t', '--threshold', default=5, type=int, help='threshold')
-parser.add_argument('-l', '--limit', default=100, type=int, help='maximal size of a clone')
+parser.add_argument('-o', '--output', default='', help='output file')
 args = parser.parse_args()
 
 fm = shellinford.FMIndex()
@@ -29,41 +29,57 @@ for d, subdirs, files in os.walk(args.dir):
                 fm.push_back(content)
 fm.build()
 
-clones = {}
+seen_clones = set()
+clone_fragments = pygtrie.Trie()
+clones = pygtrie.Trie()
+
+def add_clone(seq, v):
+    for item in clones.prefixes(seq):
+        if item[1][0] <= v[0]:
+            del clones[item[0]]
+    clones[seq] = v
+    for i in range(1, len(seq)):
+        for item in clone_fragments.prefixes(seq[i:]):
+            if item[1][0] <= v[0]:
+                del clone_fragments[item[0]]
+        clone_fragments[seq[i:]] = v
 
 def search_clone(content, i, start):
-    for j in tqdm(range(start, min(len(content) - i, args.limit))):
-        text = ''.join(content[i:i+j])
-        if text in clones:
+    for j in tqdm(range(start, len(content) - i)):
+        seq = content[i:i+j]
+        if seq in seen_clones:
             pass
+        seen_clones.add(seq)
+        text = ''.join(seq)
         docs = fm.search(text)
         c = sum([doc.count[0] for doc in docs])
         if c <= 1:
             break
-        clones[tuple(content[i:i+j])] = (c, [doc.doc_id for doc in docs])
+        frags = clone_fragments.iteritems(prefix=seq)
+        if clone_fragments.has_subtrie(seq) or seq in clone_fragments:
+            if all(map(lambda item: item[1][0] < c, frags)):
+                add_clone(seq, (c, [doc.doc_id for doc in docs]))
+            else:
+                break
+        else:
+            add_clone(seq, (c, [doc.doc_id for doc in docs]))
 
 for fild_id, f in tqdm(file_list.items()):
     with open(f, 'r') as content_file:
-        content = content_file.readlines()
+        content = tuple(content_file.readlines())
         start = args.threshold
         for i in tqdm(range(len(content) - args.threshold + 1)):
             search_clone(content, i, args.threshold)
 
-def add_subtuples(d, t, v):
-    for start in range(len(t)-1):
-        d[t[start:]] = v
+if args.output == '':
+    output = sys.stdout
+else:
+    output = open(args.output, 'w')
 
-seen_clones = pygtrie.Trie()
-keep_clones = []
-for item in sorted(clones.items(), key=lambda x: len(x[0]), reverse=True):
-    if (item[0] not in seen_clones and not seen_clones.has_subtrie(item[0])) or all(map(lambda k: seen_clones[k][0] < item[1][0], seen_clones.iterkeys(prefix=item[0]))):
-        keep_clones.append(item)
-        add_subtuples(seen_clones, item[0], item[1])
-
-for clone in keep_clones:
-    print('<<<<<<<<')
-    print('count {}'.format(clone[1][0]))
+for clone in sorted(clones.items(), key=lambda x:len(x[0]), reverse=True):
+    print('<<<<<<<<', file=output)
+    print('count {}'.format(clone[1][0]), file=output)
     for file in [file_list[id] for id in clone[1][1]]:
-        print('appears in {}'.format(file))
-    print(''.join(clone[0]))
-    print('>>>>>>>>')
+        print('appears in {}'.format(file), file=output)
+    print(''.join(clone[0]), file=output)
+    print('>>>>>>>>', file=output)
